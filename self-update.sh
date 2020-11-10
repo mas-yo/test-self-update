5d386dad4f2db790c9d18a7c20f5eec40a7dabd9
Masahiro Yoshida <myoshida@aiming-inc.com>
2020-11-10 12:15:55 +0900

    check updating

--- self-update.sh
+++ self-update.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
-. $(dirname $0)/.env
 set -xeuo
 
 function self_update
 {
+  local base_path=$(dirname $0)
+  . ${base_path}/.env
+
+  if [ -f "${base_path}/.self_updating" ]; then
+    rm -f "${base_path}/.self_updating"
+    return
+  fi
+
+  touch "${base_path}/.self_updating"
   git pull origin ${SELF_UPDATE_BRANCH}
   ./$(basename $0) $@
   exit 0
